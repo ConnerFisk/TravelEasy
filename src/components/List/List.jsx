b814b78c8bf08dccf27f2ac35f3e7c52fec1f212
ConnerFisk <u1354765@umail.utah.edu>
2022-12-31 12:27:14 -0500

    Setup groundwork code for listing the places.

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,11 @@
+/**
+ * This class holds the code for the List.
+ */
+
 import React, { useState } from "react";
 import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from "@material-ui/core";
+
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 import useStyles from './styles';
 
@@ -10,6 +16,20 @@ const List = () => {
     // for them.
     const [type, setType] = useState('restaurants');
     const [rating, setRating] = useState('');
+
+    // Dummy variables for places.
+    const places = [
+        { name: 'Place' },
+        { name: 'Pizza' },
+        { name: 'Steakhouse' },
+        { name: 'Place' },
+        { name: 'Pizza' },
+        { name: 'Steakhouse' },
+        { name: 'Place' },
+        { name: 'Pizza' },
+        { name: 'Steakhouse' },
+    ];
+
 
     return (
         <div className={ classes.container }>
@@ -35,8 +55,18 @@ const List = () => {
                     <MenuItem value={4.5}>Above 4.5</MenuItem>
                 </Select>
             </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {/* Iterate over all of the places.
+                    Since we're not deleting items, we can use this method.
+                */}
+                {places?.map((place, i) => (
+                    <Grid item key={i} xs={12}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
         </div>
-    )
+    );
 }
 
 export default List;
